8beb0c13d0a535dab6f4e01724c83613dc46e9cc
zhaorengui <zrg1390556487@gmail.com>
2016-10-24 22:46:13 +0800

    sql commit.

--- photos_20161024.sql
+++ photos_20161024.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2016-09-29 23:17:41
+Date: 2016-10-24 22:44:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -140,23 +140,15 @@ CREATE TABLE `ph_system_admin` (
   `is_enable` varchar(4) COLLATE utf8_unicode_ci DEFAULT '1' COMMENT '启用状态',
   `is_delete` varchar(4) COLLATE utf8_unicode_ci DEFAULT '0' COMMENT '删除状态',
   `remark` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '描述信息',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=350 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=351 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of ph_system_admin
 -- ----------------------------
-INSERT INTO `ph_system_admin` VALUES ('1', '0', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '男', '1390556487@qq.com', '13822221141', '1470837595', '1471136371', '127.0.0.1', '1474205364', '1', null, '超级管理员，至高无上的权利');
-INSERT INTO `ph_system_admin` VALUES ('343', '1', '34443', 'e10adc3949ba59abbe56e057f20f883e', '男', '1334@qq.com', '13533322221', '1475063324', null, null, null, '1', '0', '');
-INSERT INTO `ph_system_admin` VALUES ('344', '0', '45545', 'e10adc3949ba59abbe56e057f20f883e', '男', '1334@qq.com', '13533322221', '1475063351', null, null, null, '1', '0', '');
-INSERT INTO `ph_system_admin` VALUES ('345', '0', '1234511', 'e10adc3949ba59abbe56e057f20f883e', '男', '1334@qq.com', '13533322221', '1475063385', null, null, null, '1', '0', '');
-INSERT INTO `ph_system_admin` VALUES ('346', '0', '1234u1', 'e10adc3949ba59abbe56e057f20f883e', '女', '1334@qq.com', '13533322221', '1475063430', null, null, null, '1', '0', '');
-INSERT INTO `ph_system_admin` VALUES ('347', '0', '1234999', 'e10adc3949ba59abbe56e057f20f883e', '女', '1334@qq.com', '13533322221', '1475063456', null, null, null, '1', '0', '');
-INSERT INTO `ph_system_admin` VALUES ('348', '0', '1343414', 'e10adc3949ba59abbe56e057f20f883e', '男', '1334@qq.com', '13533322221', '1475071221', null, null, null, '1', '0', '');
-INSERT INTO `ph_system_admin` VALUES ('349', '0', '123441', 'e10adc3949ba59abbe56e057f20f883e', '男', '1334@qq.com', '13533322221', '1475071251', null, null, null, '1', '0', '');
-INSERT INTO `ph_system_admin` VALUES ('340', '0', 'ttt', 'e10adc3949ba59abbe56e057f20f883e', '男', '13441@qq.com', '13533762871', '1472292829', null, null, null, '1', '0', '');
-INSERT INTO `ph_system_admin` VALUES ('341', '1', '3341', 'e10adc3949ba59abbe56e057f20f883e', '男', '13441@qq.com', '13533762871', '1472292848', '1472354912', null, null, '0', '0', '');
-INSERT INTO `ph_system_admin` VALUES ('342', '2', '3333', 'e10adc3949ba59abbe56e057f20f883e', '男', '13441@qq.com', '13533762871', '1472293138', null, null, null, '1', '0', '');
+INSERT INTO `ph_system_admin` VALUES ('1', '0', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '男', '1390556487@qq.com', '13822221141', '1470837595', '1471136371', '127.0.0.1', '1477305038', '1', '0', '超级管理员，至高无上的权利');
+INSERT INTO `ph_system_admin` VALUES ('350', '1', 'test', 'e10adc3949ba59abbe56e057f20f883e', '男', '134@qq.com', '13566765611', '1475669519', null, null, null, '1', '0', '');
 
 -- ----------------------------
 -- Table structure for ph_system_logs
@@ -187,54 +179,57 @@ CREATE TABLE `ph_system_menu` (
   `menu_alias` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '菜单别名',
   `menu_url` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '菜单URL',
   `menu_icon` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '菜单小图标',
+  `sort` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '排序',
   `create_time` int(11) DEFAULT NULL COMMENT '添加时间',
   `create_who` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '添加人',
   `edit_time` int(11) DEFAULT NULL COMMENT '编辑时间',
   `edit_who` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '编辑人',
   `remark` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of ph_system_menu
 -- ----------------------------
-INSERT INTO `ph_system_menu` VALUES ('1', 'M0001', '0', '文章管理', 'article_manage', '', '&#xe616;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('2', 'M0002', 'M0001', '文章列表', 'article_list', 'index.php?controller=article&method=article_list', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('3', 'M0003', '0', '图片管理', 'photos_manage', '', '&#xe613;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('4', 'M0004', 'M0003', '图片列表', 'photos_list', 'index.php?controller=photos&method=photos_list', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('5', 'M0005', 'M0003', '图片分类', 'photos_category', 'index.php?controller=photos&method=photos_category', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('6', 'M0006', '0', '页面管理', 'page_manage', '', '&#xe626;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('7', 'M0007', 'M0006', '首页管理', 'page_home', 'index.php?controller=page&method=page_home', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('8', 'M0008', 'M0006', '友情链接', 'page_flinks', 'index.php?controller=page&method=page_flinks', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('9', '0', '', '相册管理平台', 'photos_platform', 'index.php', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('10', 'M0009', '0', '广告管理', 'advert_manage', null, '&#xe613;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('11', 'M0011', 'M0009', '广告分类', 'advert_category', 'index.php?controller=advert&method=advert_category', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('12', 'M0010', 'M0009', '广告列表', 'advert_list', 'index.php?controller=advert&method=advert_list', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('13', 'M0012', '0', '评论管理', 'comment_manage', null, '&#xe622;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('14', 'M0013', 'M0012', '评论列表', 'comment_list', 'index.php?controller=comment&method=comment_list', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('15', 'M0014', 'M0012', '意见反馈', 'feedback', 'index.php?controller=comment&method=feedback', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('16', 'M0015', '0', '财务管理', 'finance_manage', null, '&#xe63a;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('17', 'M0016', 'M0015', '订单列表', 'order_list', 'index.php?controller=comment&method=order_list', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('18', 'M0017', 'M0015', '充值管理', 'recharge_manage', 'index.php?controller=comment&method=recharge_manage', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('19', 'M0018', 'M0015', '发票管理', 'invoice_manage', 'index.php?controller=comment&method=invoice_manage', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('20', 'M0019', '0', '会员管理', 'member_manage', null, '&#xe60d;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('21', 'M0020', 'M0019', '会员列表', 'member_list', 'index.php?controller=comment&method=member_list', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('22', 'M0021', 'M0019', '等级管理', 'member_level', 'index.php?controller=comment&method=member_level', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('23', 'M0022', 'M0019', '积分管理', 'member_score', 'index.php?controller=comment&method=member_score', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('24', 'M0023', 'M0019', '浏览记录', 'member_record_browse', 'index.php?controller=comment&method=member_score', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('25', 'M0024', 'M0019', '下载记录', 'member_record_download', 'index.php?controller=comment&method=member_record_browse', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('26', 'M0025', 'M0019', '分享记录', 'member_record_share', 'index.php?controller=comment&method=member_record_share', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('27', 'M0026', '0', '管理员管理', 'admin_manage', null, '&#xe62d;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('28', 'M0027', 'M0026', '管理员列表', 'admin_list', 'index.php?controller=admin&method=admin_list', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('29', 'M0028', 'M0026', '角色管理', 'admin_role', 'index.php?controller=admin&method=admin_role', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('30', 'M0029', 'M0026', '权限管理', 'admin_permission', 'index.php?controller=admin&method=admin_permission', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('31', 'M0030', '0', '统计分析', 'statistics_analyse', null, '&#xe61a;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('32', 'M0031', '0', '系统管理', 'system_manage', null, '&#xe62e;', null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('33', 'M0032', 'M0031', '系统设置', 'system_setting', 'index.php?controller=system&method=system_setting', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('34', 'M0033', 'M0031', '栏目（菜单）管理', 'system_menu_manage', 'index.php?controller=system&method=system_menu_manage', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('35', 'M0034', 'M0031', '数据字典', 'system_data', 'index.php?controller=system&method=system_data', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('36', 'M0035', 'M0031', '屏蔽词', 'system_shielding', 'index.php?controller=system&method=system_shielding', null, null, null, null, null, null);
-INSERT INTO `ph_system_menu` VALUES ('37', 'M0036', 'M0031', '系统日志', 'system_log', 'index.php?controller=system&method=system_log', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('1', 'M0001', '0', '文章管理', 'article_manage', '', '&#xe616;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('2', 'M0002', 'M0001', '文章列表', 'article_list', 'index.php?controller=article&method=article_list', null, '1', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('3', 'M0003', '0', '图片管理', 'picture_manage', '', '&#xe613;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('4', 'M0004', 'M0003', '图片列表', 'picture_list', 'index.php?controller=picture&method=picture_list', null, '1', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('5', 'M0005', 'M0003', '图片分类', 'picture_category', 'index.php?controller=picture&method=picture_category', null, '2', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('6', 'M0006', '0', '页面管理', 'page_manage', '', '&#xe626;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('7', 'M0007', 'M0006', '首页管理', 'page_home', 'index.php?controller=page&method=page_home', null, '1', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('8', 'M0008', 'M0006', '友情链接', 'page_flinks', 'index.php?controller=page&method=page_flinks', null, '2', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('9', '0', '', '相册管理平台', 'photos_platform', 'index.php', null, null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('10', 'M0009', '0', '广告管理', 'advert_manage', null, '&#xe613;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('11', 'M0011', 'M0009', '广告分类', 'advert_category', 'index.php?controller=advert&method=advert_category', null, '1', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('12', 'M0010', 'M0009', '广告列表', 'advert_list', 'index.php?controller=advert&method=advert_list', null, '2', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('13', 'M0012', '0', '评论管理', 'comment_manage', null, '&#xe622;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('14', 'M0013', 'M0012', '评论列表', 'comment_list', 'index.php?controller=comment&method=comment_list', null, '1', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('15', 'M0014', 'M0012', '意见反馈', 'feedback', 'index.php?controller=comment&method=feedback', null, '2', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('16', 'M0015', '0', '财务管理', 'finance_manage', null, '&#xe63a;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('17', 'M0016', 'M0015', '订单列表', 'order_list', 'index.php?controller=comment&method=order_list', null, '1', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('18', 'M0017', 'M0015', '充值管理', 'recharge_manage', 'index.php?controller=comment&method=recharge_manage', null, '2', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('19', 'M0018', 'M0015', '发票管理', 'invoice_manage', 'index.php?controller=comment&method=invoice_manage', null, '3', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('20', 'M0019', '0', '会员管理', 'member_manage', null, '&#xe60d;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('21', 'M0020', 'M0019', '会员列表', 'member_list', 'index.php?controller=member&method=member_list', null, '1', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('22', 'M0021', 'M0019', '等级管理', 'member_level', 'index.php?controller=member&method=member_level', null, '3', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('23', 'M0022', 'M0019', '积分管理', 'member_score', 'index.php?controller=member&method=member_score', null, '4', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('24', 'M0023', 'M0019', '浏览记录', 'member_browse', 'index.php?controller=member&method=member_browse', null, '5', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('25', 'M0024', 'M0019', '下载记录', 'member_download', 'index.php?controller=member&method=member_download', null, '6', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('26', 'M0025', 'M0019', '分享记录', 'member_share', 'index.php?controller=member&method=member_share', null, '7', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('27', 'M0026', '0', '管理员管理', 'admin_manage', null, '&#xe62d;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('28', 'M0027', 'M0026', '管理员列表', 'admin_list', 'index.php?controller=admin&method=admin_list', null, '1', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('29', 'M0028', 'M0026', '角色管理', 'admin_role', 'index.php?controller=admin&method=admin_role', null, '3', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('30', 'M0029', 'M0026', '权限管理', 'admin_permission', 'index.php?controller=admin&method=admin_permission', null, '4', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('31', 'M0030', '0', '统计分析', 'statistics_analyse', null, '&#xe61a;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('32', 'M0031', '0', '系统管理', 'system_manage', null, '&#xe62e;', null, null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('33', 'M0032', 'M0031', '系统设置', 'system_setting', 'index.php?controller=system&method=system_setting', null, '1', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('34', 'M0033', 'M0031', '栏目（菜单）管理', 'system_menu', 'index.php?controller=system&method=system_menu', null, '2', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('35', 'M0034', 'M0031', '数据字典', 'system_data', 'index.php?controller=system&method=system_data', null, '3', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('36', 'M0035', 'M0031', '屏蔽词', 'system_shielding', 'index.php?controller=system&method=system_shielding', null, '4', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('37', 'M0036', 'M0031', '系统日志', 'system_log', 'index.php?controller=system&method=system_log', null, '5', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('38', 'M0037', 'M0026', '管理员回收站', 'admin_trash', 'index.php?controller=admin&method=admin_trash', null, '2', null, null, null, null, null);
+INSERT INTO `ph_system_menu` VALUES ('39', 'M0038', 'M0019', '会员回收站', 'member_trash', 'index.php?controller=member&method=member_trash', null, '2', null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for ph_system_node
@@ -242,7 +237,7 @@ INSERT INTO `ph_system_menu` VALUES ('37', 'M0036', 'M0031', '系统日志', 'sy
 DROP TABLE IF EXISTS `ph_system_node`;
 CREATE TABLE `ph_system_node` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `node_code` int(11) NOT NULL COMMENT '节点ID',
+  `node_code` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT '节点ID',
   `parent_node_code` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `node_name` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `create_time` int(11) DEFAULT NULL COMMENT '关系创建时间',
@@ -250,7 +245,8 @@ CREATE TABLE `ph_system_node` (
   `edit_time` int(11) DEFAULT NULL,
   `edit_who` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `remarks` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `node_code` (`node_code`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
@@ -265,8 +261,10 @@ CREATE TABLE `ph_system_node_permission` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `node_code` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '节点编码',
   `privilege_code` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '权限编码',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  PRIMARY KEY (`id`),
+  KEY `node_code` (`node_code`),
+  KEY `privilege_code` (`privilege_code`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of ph_system_node_permission
@@ -315,7 +313,7 @@ CREATE TABLE `ph_system_resources` (
 DROP TABLE IF EXISTS `ph_system_roles`;
 CREATE TABLE `ph_system_roles` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `role_id` int(11) DEFAULT NULL COMMENT '角色ID',
+  `role_id` int(11) NOT NULL COMMENT '角色ID',
   `role_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT '角色名称',
   `role_alias` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '角色描述',
@@ -338,9 +336,11 @@ DROP TABLE IF EXISTS `ph_system_role_permission`;
 CREATE TABLE `ph_system_role_permission` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `role_id` int(11) DEFAULT NULL COMMENT '角色ID',
-  `privilege_code` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '权限编码',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `privilege_code` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '权限ID',
+  `access_privilege_code` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '访问权限编码',
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of ph_system_role_permission
